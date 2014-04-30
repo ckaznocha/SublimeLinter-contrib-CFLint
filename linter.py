4bb78cd1ab914baa7399c6540598e5a21d4c8fe9
ckaznocha <ckaznocha@gmail.com>
2014-04-30 00:12:42 -0400

    Initial commit

---
+++ linter.py
@@ -0,0 +1,25 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by ckaznocha
+# Copyright (c) 2014 ckaznocha
+#
+# License: MIT
+#
+
+"""This module exports the CFLint plugin class."""
+from SublimeLinter.lint import Linter, util
+
+class CFLint(Linter):
+
+    """Provides an interface to CFLint."""
+
+    syntax = ('coldfusioncfc','html+cfml')
+    cmd = ('cflint -q -text -file')
+    regex = r'''(?xi)
+        ^\s*Line:(?P<line>\d+)$\r?\n
+        ^\s*message:(?P<message>.+)$
+    '''
+    multiline = True
+    tempfile_suffix = '-'
